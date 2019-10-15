fe860f73eafeff1acdb6742eb83abf88f0d43ae0
B1Z0N <kolausf@gmail.com>
2019-10-16 00:38:35 +0300

    Changed database fields attributes

--- database/workspide_tables.sql
+++ database/workspide_tables.sql
@@ -1,55 +1,55 @@
 CREATE TABLE `User` (
-  `id` int PRIMARY KEY,
+  `id` int PRIMARY KEY NOT NULL,
   `first_name` varchar(30),
   `last_name` varchar(30),
-  `email` varchar(30),
-  `pass_hash` varchar(50)
+  `email` varchar(30) NOT NULL,
+  `pass_hash` varchar(50) NOT NULL
 );
 
 CREATE TABLE `Resume` (
-  `id` int PRIMARY KEY,
-  `uid` int,
-  `title` varchar(30),
-  `description` varchar(10000),
+  `id` int PRIMARY KEY NOT NULL,
+  `uid` int NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(10000) NOT NULL,
   `salary` int,
   `currency` ENUM ('usd', 'uah', 'eur')
 );
 
 CREATE TABLE `Vacancy` (
-  `id` int PRIMARY KEY,
-  `uid` int,
-  `title` varchar(30),
-  `description` varchar(10000),
+  `id` int PRIMARY KEY NOT NULL,
+  `uid` int NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(10000) NOT NULL,
   `salary` int,
   `currency` ENUM ('usd', 'uah', 'eur'),
   `experience` tinyint
 );
 
 CREATE TABLE `Connector` (
-  `id` int PRIMARY KEY,
-  `uid` int,
-  `ad_t` ENUM ('resume', 'vacancy'),
-  `ad_id` int
+  `id` int PRIMARY KEY NOT NULL,
+  `uid` int NOT NULL,
+  `ad_t` ENUM ('resume', 'vacancy') NOT NULL,
+  `ad_id` int NOT NULL
 );
 
 CREATE TABLE `PetProject` (
-  `id` int PRIMARY KEY,
-  `resume_id` int,
-  `title` varchar(30),
-  `link` varchar(30)
+  `id` int PRIMARY KEY NOT NULL,
+  `resume_id` int NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `link` varchar(30) NOT NULL
 );
 
 CREATE TABLE `Skills` (
-  `id` int PRIMARY KEY,
-  `ad_t` ENUM ('resume', 'vacancy'),
-  `ad_id` int,
-  `skill` varchar(30)
+  `id` int PRIMARY KEY NOT NULL,
+  `ad_t` ENUM ('resume', 'vacancy') NOT NULL,
+  `ad_id` int NOT NULL,
+  `skill` varchar(30) NOT NULL
 );
 
 CREATE TABLE `Responsibilities` (
-  `id` int PRIMARY KEY,
-  `vacancy_id` int,
-  `responsibility` varchar(30)
+  `id` int PRIMARY KEY NOT NULL,
+  `vacancy_id` int NOT NULL,
+  `responsibility` varchar(30) NOT NULL
 );
 
 ALTER TABLE `User` ADD FOREIGN KEY (`id`) REFERENCES `Connector` (`uid`);
