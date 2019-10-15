d8b6d2755454edd780af169ce8ea76bc8798cfdd
B1Z0N <kolausf@gmail.com>
2019-10-15 23:49:38 +0300

    Connected database

--- workspide/workspide/settings.py
+++ workspide/workspide/settings.py
@@ -74,9 +74,14 @@ WSGI_APPLICATION = 'workspide.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # database, configured locally
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # database engine
+        'NAME': 'workspide', # database name
+        'USER': 'workspide_admin', # database user
+        'PASSWORD': 'workspide_password', # database password
+        'HOST': 'localhost',   # Or an IP Address of database
+        'PORT': '3306', # default database port
     }
 }
 
