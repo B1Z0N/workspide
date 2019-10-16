106632efe64d70db3aa1c4cdac00d933c1a5d946
B1Z0N <kolausf@gmail.com>
2019-10-16 22:05:04 +0300

    Fixed dfd

--- database/workspide_tables.sql
+++ database/workspide_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE `User` (
   `first_name` varchar(30),
   `last_name` varchar(30),
   `email` varchar(30) NOT NULL,
-  `pass_hash` varchar(50) NOT NULL
+  `pass_hash` varchar(64) NOT NULL
 );
 
 CREATE TABLE `Resume` (
@@ -22,14 +22,19 @@ CREATE TABLE `Vacancy` (
   `description` varchar(10000) NOT NULL,
   `salary` int,
   `currency` ENUM ('usd', 'uah', 'eur'),
-  `experience` tinyint
+  `experience_months` int
 );
 
-CREATE TABLE `Connector` (
+CREATE TABLE `SkillForResume` (
   `id` int PRIMARY KEY NOT NULL,
-  `uid` int NOT NULL,
-  `ad_t` ENUM ('resume', 'vacancy') NOT NULL,
-  `ad_id` int NOT NULL
+  `resume_id` int NOT NULL,
+  `text` varchar(100) NOT NULL
+);
+
+CREATE TABLE `SkillForVacancy` (
+  `id` int PRIMARY KEY NOT NULL,
+  `vacancy_id` int NOT NULL,
+  `text` varchar(100) NOT NULL
 );
 
 CREATE TABLE `PetProject` (
@@ -39,52 +44,20 @@ CREATE TABLE `PetProject` (
   `link` varchar(30) NOT NULL
 );
 
-CREATE TABLE `Skills` (
-  `id` int PRIMARY KEY NOT NULL,
-  `ad_t` ENUM ('resume', 'vacancy') NOT NULL,
-  `ad_id` int NOT NULL,
-  `skill` varchar(30) NOT NULL
-);
-
 CREATE TABLE `Responsibilities` (
   `id` int PRIMARY KEY NOT NULL,
   `vacancy_id` int NOT NULL,
   `responsibility` varchar(30) NOT NULL
 );
 
-ALTER TABLE
-  `User`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Connector` (`uid`);
+ALTER TABLE `User` ADD FOREIGN KEY (`id`) REFERENCES `Resume` (`uid`);
 
-ALTER TABLE
-  `Vacancy`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Connector` (`ad_id`);
+ALTER TABLE `User` ADD FOREIGN KEY (`id`) REFERENCES `Vacancy` (`uid`);
 
-ALTER TABLE
-  `Resume`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Connector` (`ad_id`);
+ALTER TABLE `Resume` ADD FOREIGN KEY (`id`) REFERENCES `PetProject` (`resume_id`);
 
-ALTER TABLE
-  `Resume`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Skills` (`ad_id`);
+ALTER TABLE `Resume` ADD FOREIGN KEY (`id`) REFERENCES `SkillForResume` (`resume_id`);
 
-ALTER TABLE
-  `Vacancy`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Skills` (`ad_id`);
+ALTER TABLE `Vacancy` ADD FOREIGN KEY (`id`) REFERENCES `SkillForVacancy` (`vacancy_id`);
 
-ALTER TABLE
-  `Vacancy`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Responsibilities` (`vacancy_id`);
-
-ALTER TABLE
-  `PetProject`
-ADD
-  FOREIGN KEY (`id`) REFERENCES `Resume` (`id`);
-
-# DROP TABLE Connector, User, Responsibilities, Skills, Vacancy, PetProject, Resume; # - uncomment to drop all
+ALTER TABLE `Vacancy` ADD FOREIGN KEY (`id`) REFERENCES `Responsibilities` (`vacancy_id`);
