bb0bf718dbf35110cf3b44029c0c2bc096e927a2
B1Z0N <kolausf@gmail.com>
2019-10-19 12:52:31 +0300

    Added url routing for login/logout

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
-from . import search_views
+from django.contrib.auth import login
+from . import search_views, account_views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('login/', TemplateView.as_view(template_name='login.html')),
+    
+    path('signup/register/', account_views.register),
+    # path('logout/', ),
+    path('signup/', include('django.contrib.auth.urls')),
     path('account/', TemplateView.as_view(template_name='account.html')),
+
     path('search/<str:_type>/<str:_text>/', search_views.search),
     path('search/<str:_type>/', search_views.empty_search),
 ]
