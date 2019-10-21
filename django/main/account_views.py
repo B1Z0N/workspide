acc000eba34d704cb57c1d541113536ad72047ff
B1Z0N <kolausf@gmail.com>
2019-10-21 22:38:02 +0300

    Added form to delete account

--- django/main/account_views.py
+++ django/main/account_views.py
@@ -6,23 +6,41 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.http import Http404, HttpResponseRedirect
 
+from django.views import View
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import UserCreationForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+
 from .forms import UserCreationForm as RegisterForm, EmailChangeForm, NameChangeForm
-from .models import User
+
+from django.contrib.auth import get_user_model, login, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from .tokens import account_activation_token
 
 
-def register(request):
-    form = RegisterForm()
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(email=email, password=raw_password)
-            login(request, user)
-            return redirect('/')
+User = get_user_model()
 
-    return render(request, 'registration/register.html', {'form': form})
+
+# def register(request):
+#     form = RegisterForm()
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             email = form.cleaned_data.get('email')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(email=email, password=raw_password)
+#             login(request, user)
+#             return redirect('/')
+
+#     return render(request, 'registration/register.html', {'form': form})
 
 
 def account(request):
@@ -62,3 +80,56 @@ def account(request):
         'delete_account' : delete_account,
     })
 
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            # Create an inactive user with no password:
+            user = form.save(commit=False)
+            user.is_active = False
+            user.set_unusable_password()
+            user.save()
+
+            # Send an email to the user with the token:
+            mail_subject = 'Activate your account.'
+            current_site = get_current_site(request)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = account_activation_token.make_token(user)
+            activation_link = "{0}/?uid={1}&token{2}".format(current_site, uid, token)
+            message = "Hi, activate your account: \n {0}".format(activation_link)
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, message, to=[to_email])
+            email.send()
+        
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'registration/register.html', {'form': form})
+
+
+class Activate(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user is not None and account_activation_token.check_token(user, token):
+            # activate user and login:
+            user.is_active = True
+            user.save()
+            login(request, user)
+
+            form = PasswordChangeForm(request.user)
+            return render(request, 'activation.html', {'form': form})
+
+        else:
+            return HttpResponse('Activation link is invalid!')
+    def post(self, request):
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user) # Important, to update the session with the new password
+            return HttpResponse('Password changed successfully')
