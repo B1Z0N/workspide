05406fb7ec4f9c7c007bfff2ec8268b1d879a65f
B1Z0N <kolausf@gmail.com>
2019-10-22 13:20:58 +0300

    Created class for sending mails

---
+++ django/main/email.py
@@ -0,0 +1,56 @@
+"""
+    Handling of token links creation and sending emails
+"""
+
+# for generating of activation/token links
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils import six
+import enum
+
+# for sending mails
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+
+class TokenLinkType(enum.Enum):
+    mail_activation = 1
+    account_deletion = 2
+    password_change = 3
+
+
+class TokenGenerator(PasswordResetTokenGenerator):
+
+    def __init__(self, token_link_type=TokenLinkType.mail_activation):
+        self.type = token_link_type
+
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.is_active) + six.text_type(self.type)
+        )
+
+
+account_activation_token = TokenGenerator(TokenLinkType.mail_activation)
+account_deletion_token = TokenGenerator(TokenLinkType.account_deletion)
+password_change_token = TokenGenerator(TokenLinkType.password_change)
+
+
+class MailSender:
+
+    def __init__(self, template_path='emails/base.html'):
+            self.EMAIL_TEMPLATE = get_template(template_path)
+
+    def __call__(self, subject, message, link, link_text, to_email):
+        message = self.EMAIL_TEMPLATE.render(
+            {
+                'message' : message, 
+                'link' : link, 
+                'link_text' : link_text,
+            }
+        )
+        email = EmailMultiAlternatives(subject, '', settings.EMAIL_HOST_USER, [to_email])
+        email.attach_alternative(message, "text/html")
+        email.send()
+
+
+send_mail = MailSender()
