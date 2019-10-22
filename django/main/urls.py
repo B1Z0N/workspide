e15086146456303afd0caeb6d7ba125dbce93a95
B1Z0N <kolausf@gmail.com>
2019-10-22 10:29:58 +0300

    Renamed, reordered urls, optimized imports

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.contrib.auth import login
+
 from . import search_views, account_views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     
     path('signup/register/', account_views.register, name='register'),
-    path('signup/activate_mail/<str:uidb64>/<str:token>', account_views.activate_mail, name='activate'),
-    path('account/delete/<str:uidb64>/<str:token>', account_views.delete_account, name='activate'),
     path('signup/', include('django.contrib.auth.urls')),
-    path('account/', account_views.account),
+    path('account/', account_views.account, name='account'),
 
-    path('search/<str:_type>/<str:_text>/', search_views.search),
-    path('search/<str:_type>/', search_views.empty_search),
+    path('signup/activate_mail/<str:uidb64>/<str:token>', account_views.activate_mail, name='activate_main'),
+    path('account/delete/<str:uidb64>/<str:token>', account_views.delete_account, name='submit_deletion'),
+
+    path('search/<str:_type>/<str:_text>/', search_views.search, name='search_text'),
+    path('search/<str:_type>/', search_views.empty_search, name='search_default'),
 ]
