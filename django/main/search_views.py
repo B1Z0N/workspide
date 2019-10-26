c04b5455409d3c82ee6ad0164b7ffc8780bbbed2
B1Z0N <kolausf@gmail.com>
2019-10-26 17:21:54 +0300

    Completed retrieval of ad

--- django/main/search_views.py
+++ django/main/search_views.py
@@ -1,33 +1,30 @@
 from django.shortcuts import render
 from django.http import Http404
-import json
+from django.conf import settings
+import random
 
 from .forms import AdModelForm, SKillModelForm, PetProjectModelForm, ResponsibilityModelForm
 
 
 def search(request, _type, _text):
-    if _type != 'jobs' and _type != 'workers':
+    if _type != 'jobs' and _type != 'employees':
         raise Http404()
 
-    return render(
-        request, 
-        'search.html',
-        # json.dumps(
-            {'search_type' : _type, 
-            'search_text' : _text}
-        # ),
-    )
+    return render(request, 'search.html', {
+            'search_type' : _type, 
+            'search_text' : _text
+    })
 
 
 def empty_search(request, _type):
     return search(request, _type, "")
 
 
-def add_ad(ad_type):
+# just a summernote placeholder generator
+def set_description_placeholder(ad_type):
     assert(ad_type == "vacancy" or ad_type == "resume")
 
     def gen_random_job():
-        import random
         ABSOLUTELY_ONE_HUNDRED_PERCENT_NOT_WEIRD_JOBS = [
             'sleeper',
             'drying paint watcher',
@@ -41,26 +38,56 @@ def add_ad(ad_type):
             'scuba diving pizza delivery man',
         ]
         return random.choice(ABSOLUTELY_ONE_HUNDRED_PERCENT_NOT_WEIRD_JOBS)
+
+    def gen_random_resume_beginning():
+        RESUME_BEGINNING = [
+            'Hallo there. I`m a professional',
+            'Greetings. I`m a passionate',
+            'Aloha. I`m a highly qualified',
+            'Ni hao. Wo hen zhuanye',
+            'Hi. I see myself as',
+            'Sometimes I just go to the beach, look at the waves and feel thankfull that I am a',
+        ]
+        return random.choice(RESUME_BEGINNING)
+
+    def gen_radnom_vacancy_beginning():
+        VACANCY_BEGINNING = [
+            'Serious company is looking for serious(not funny)',
+            'We found ourselves looking for an employee like',
+            'Looking for experienced',
+            'Posting job for a position of ',
+            'Can`t wait to see YOU at position of'
+        ]
+        return random.choice(VACANCY_BEGINNING)
+
+    def set_summernote_placeholder(placeholder):
+        settings.SUMMERNOTE_CONFIG['summernote']['placeholder'] = placeholder 
     
+    if ad_type == 'vacancy':
+        set_summernote_placeholder(
+            ' '.join([gen_radnom_vacancy_beginning(), gen_random_job()])
+        )
+    else:
+        set_summernote_placeholder(
+            ' '.join([gen_random_resume_beginning(), gen_random_job()])
+        )
+
+
+def add_ad(ad_type):
+    assert(ad_type == "vacancy" or ad_type == "resume")
+
     def actual_view(request):
         form = AdModelForm()
 
-        if request.method == 'POST':
-            if 'submit_btn' in request.POST:
-                form = AdModelForm(instance=request.user, data=request.POST)
-                if form.is_valid():
-                    form.cleaned_data['ad_type'] = ad_type
-        
-        context = { 
-            'vacancy' : 'True',
-            'title' : 'New vacancy',
-            'text_placeholder' : 'Looking for experienced ' + gen_random_job(),
-        } if ad_type == "vacancy" else { 
-            'resume' : 'True',
-            'title' : 'New resume',
-            'text_placeholder' : 'Hello there, i am professional ' + gen_random_job(),
+        if request.method == 'POST' and 'submit_button' in request.POST:
+            pass
+
+        context = {
+            'form' : form,
+            ad_type : 'True',
+            'title' : 'New ' + ad_type,
         }
-        context['form'] = form
+        set_description_placeholder(ad_type)
         
         return render(request, 'ads/add_ad.html', context)
 
