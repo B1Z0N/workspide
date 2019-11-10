3668504393810c1c1c5486376257858ab12fb374
B1Z0N <kolausf@gmail.com>
2019-11-10 19:13:08 +0200

    Refactored to new version of views

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
-from django.shortcuts import render, redirect
-from . import search_views, account_views
+from django.shortcuts import render
+from main.views import signup_views, account_views, ad_views, pide_views, search_views
 
 
-def unauthorized_access(func):
+# function that denies unauthorized access to view
+def unauthorized_access(view):
     def _(request, *args, **kwargs):
         if not request.user.is_authenticated:
             return render(request, 'alerts/render_base.html',
@@ -15,55 +15,64 @@ def unauthorized_access(func):
                 'response_error_text': 'Unauthorized access, go login or register',
             })
         else:
-            return func(request, *args, **kwargs)
+            return view(request, *args, **kwargs)
     return _
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+     # site base view
+     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 
-    path('signup/register/', account_views.register, name='register'),
-    path('signup/activate_mail/<str:uidb64>/<str:token>',
-         account_views.activate_mail, name='activate_mail'),
-    # mainly here for password reset
-    path('signup/', include('django.contrib.auth.urls')),
 
-    path('account/', unauthorized_access(account_views.account), name='account'),
-    path('account/delete/<str:uidb64>/<str:token>',
+     # register/login/activate views
+     path('signup/register/', signup_views.register, name='register'),
+     path('signup/activate_mail/<str:uidb64>/<str:token>',
+         signup_views.activate_mail, name='activate_mail'),
+     # mainly here for password reset
+     path('signup/', include('django.contrib.auth.urls')),
+
+
+     # account views
+     path('account/', unauthorized_access(account_views.account), name='account'),
+     path('account/delete/<str:uidb64>/<str:token>',
          unauthorized_access(account_views.delete_account), name='submit_deletion'),
-    path('account/email_change_complete/<str:uidb64>/<str:emailb64>/<str:token>',
+     path('account/email_change_complete/<str:uidb64>/<str:emailb64>/<str:token>',
          unauthorized_access(account_views.email_change_complete), name='submit_deletion'),
-    path('account/password_change/',
+     path('account/password_change/',
          unauthorized_access(account_views.password_change), name='password_change'),
-    path('account/email_change/<str:uidb64>/<str:token>',
+     path('account/email_change/<str:uidb64>/<str:token>',
          unauthorized_access(account_views.email_change), name='email_change'),
-    path('account/password_reset/',
+     path('account/password_reset/',
         auth_views.PasswordResetView.as_view(
             subject_template_name='emails/password_reset_subject.txt',
             html_email_template_name='emails/password_reset.html',
         )
     ),
 
-    path('account/add_resume/',
-         unauthorized_access(search_views.add_ad("resume")), name="add_resume"),
-    path('account/add_vacancy/',
-         unauthorized_access(search_views.add_ad("vacancy")), name="add_vacancy"),
-    path('account/delete_ad/<int:ad_id>/',
-         unauthorized_access(search_views.delete_ad), name='delete_ad'),
-    path('account/edit_resume/<int:ad_id>/',
-         unauthorized_access(search_views.edit_ad('resume')), name='edit_resume'),
-    path('account/edit_vacancy/<int:ad_id>/',
-         unauthorized_access(search_views.edit_ad('vacancy')), name='edit_vacancy'),
 
-    path('show_ad/<int:ad_id>/', search_views.show_ad, name='show_ad'),
-    path('pide/<int:ad_id>/', search_views.pide, name='pide'),
-    path('pide_confirm/<int:pide_id>/',
-         unauthorized_access(search_views.pide_confirm), name='pide_confirm'),
-    path('feed/', unauthorized_access(search_views.feed), name='feed'),
+     # ad create/delete/edit views
+     path('show_ad/<int:ad_id>/', ad_views.show_ad, name='show_ad'),
+     path('account/add_resume/',
+         unauthorized_access(ad_views.add_ad("resume")), name="add_resume"),
+     path('account/add_vacancy/',
+         unauthorized_access(ad_views.add_ad("vacancy")), name="add_vacancy"),
+     path('account/delete_ad/<int:ad_id>/',
+         unauthorized_access(ad_views.delete_ad), name='delete_ad'),
+     path('account/edit_resume/<int:ad_id>/',
+         unauthorized_access(ad_views.edit_ad('resume')), name='edit_resume'),
+     path('account/edit_vacancy/<int:ad_id>/',
+         unauthorized_access(ad_views.edit_ad('vacancy')), name='edit_vacancy'),
 
 
+     # pide(deal) views
+     path('pide/<int:ad_id>/', pide_views.pide, name='pide'),
+     path('pide_confirm/<int:pide_id>/',
+         unauthorized_access(pide_views.pide_confirm), name='pide_confirm'),
+     path('feed/', unauthorized_access(pide_views.feed), name='feed'),
 
-    path(
+
+     # one big filter search view
+     path(
         '/'.join([
             'search',
             'type_<str:_type>',
@@ -86,5 +95,6 @@ urlpatterns = [
             'order_by_<str:_order_by>/',
             ]), search_views.search, name = 'filtered_search'),
 
-    path('search/type_<str:_type>/text_<str:_text>/', search_views.search, name="text_search"),
+     # text search
+     path('search/type_<str:_type>/text_<str:_text>/', search_views.search, name="text_search"),
 ]
